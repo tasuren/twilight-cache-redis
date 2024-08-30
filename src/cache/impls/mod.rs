7025bf4bd963d063d4bb14472be994196f4f7c95
Takagi Tasuku <tasuren@outlook.jp>
2024-08-30 13:04:07 +0900

    Implement getter for `CurrentUser` cache

--- src/cache/impls/mod.rs
+++ src/cache/impls/mod.rs
@@ -1,8 +1,8 @@
-use twilight_model::user::CurrentUser;
+use redis::AsyncCommands;
 
-use crate::{CacheStrategy, Error};
+use crate::{CacheStrategy, Connection, Error, RedisCache};
 
-use super::{Pipe, RedisKey, ToCachedRedisArg};
+use super::{FromCachedRedisValue, Pipe, RedisKey, ToCachedRedisArg};
 
 mod channel;
 mod emoji;
@@ -13,10 +13,25 @@ mod presence;
 mod reaction;
 mod user;
 
+impl<S: CacheStrategy> RedisCache<S> {
+    pub async fn get_current_user(
+        &self,
+        conn: &mut Connection<'_>,
+    ) -> Result<S::CurrentUser, Error> {
+        let raw: redis::Value = conn.get(RedisKey::CurrentUser).await?;
+        S::CurrentUser::from_cached_redis_value(&raw)
+    }
+}
+
 impl<S: CacheStrategy> Pipe<S> {
+    pub fn get_current_user(&mut self) -> &mut Self {
+        self.0.get(RedisKey::CurrentUser);
+        self
+    }
+
     pub(crate) fn set_current_user(
         &mut self,
-        current_user: CurrentUser,
+        current_user: S::CurrentUser,
     ) -> Result<&mut Self, Error> {
         self.0
             .set(RedisKey::CurrentUser, current_user.to_redis_arg()?);
